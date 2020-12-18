cac6a3b370fbf5188268406e0ca102000f77c936
Ubuntu <ubuntu@management.ceph.internal>
2020-12-18 14:42:29 +0000

    remove management host and update init

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-update-alternatives --set editor  /usr/bin/vim.basic
+sudo update-alternatives --set editor  /usr/bin/vim.basic
 sudo add-apt-repository ppa:ansible/ansible
 sudo apt update
-sudo apt install ansible
+sudo apt install ansible python-netaddr
+sudo rm /etc/apt/sources.list.d/*
 
+echo "Now run: ansible-playbook --private-key ../ceph-setup_id_rsa -i hosts site.yml"
