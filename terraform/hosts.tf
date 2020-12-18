c65a0d1ed10a1fba4c0e47d4305a1075feab9758
Jesper Krogh <jesper@krogh.cc>
2020-12-18 08:42:14 +0000

    also add osd nodes to the setup

--- terraform/hosts.tf
+++ terraform/hosts.tf
@@ -50,3 +50,57 @@ resource "aws_instance" "mon3" {
   associate_public_ip_address = false
   user_data = "#!/bin/bash\nsudo  hostname mon3\nsed -i 's/compute.internal/compute.internal ceph.internal/' /etc/resolv.conf\n"
 }
+
+
+resource "aws_instance" "osd1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "i3.large"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  subnet_id = aws_subnet.ceph.id
+  key_name = aws_key_pair.terraformcluster.key_name
+  tags = {
+        Name = "Ceph OSD 1"
+  }
+  associate_public_ip_address = false
+  user_data = "#!/bin/bash\nsudo hostname osd1\nsed -i 's/compute.internal/compute.internal ceph.internal/' /etc/resolv.conf\ncp /etc/resolv.conf /tmp; rm /etc/resolv.conf; mv /tmp/resolv.conf /etc\n"
+}
+
+resource "aws_instance" "osd2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "i3.large"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  subnet_id = aws_subnet.ceph.id
+  key_name = aws_key_pair.terraformcluster.key_name
+  tags = {
+        Name = "Ceph OSD 2"
+  }
+  associate_public_ip_address = false
+  user_data = "#!/bin/bash\nsudo hostname osd2\nsed -i 's/compute.internal/compute.internal ceph.internal/' /etc/resolv.conf\ncp /etc/resolv.conf /tmp; rm /etc/resolv.conf; mv /tmp/resolv.conf /etc\n"
+}
+
+resource "aws_instance" "osd3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "i3.large"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  subnet_id = aws_subnet.ceph.id
+  key_name = aws_key_pair.terraformcluster.key_name
+  tags = {
+        Name = "Ceph OSD 3"
+  }
+  associate_public_ip_address = false
+  user_data = "#!/bin/bash\nsudo hostname osd3\nsed -i 's/compute.internal/compute.internal ceph.internal/' /etc/resolv.conf\ncp /etc/resolv.conf /tmp; rm /etc/resolv.conf; mv /tmp/resolv.conf /etc\n"
+}
+
+resource "aws_instance" "osd4" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "i3.large"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  subnet_id = aws_subnet.ceph.id
+  key_name = aws_key_pair.terraformcluster.key_name
+  tags = {
+        Name = "Ceph OSD 4"
+  }
+  associate_public_ip_address = false
+  user_data = "#!/bin/bash\nsudo hostname osd4\nsed -i 's/compute.internal/compute.internal ceph.internal/' /etc/resolv.conf\ncp /etc/resolv.conf /tmp; rm /etc/resolv.conf; mv /tmp/resolv.conf /etc\n"
+}
+
