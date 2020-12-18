519a4e0b7828b09e0cd0ab97a4ab06d0956dac5d
Jesper Krogh <jesper@krogh.cc>
2020-12-18 08:08:18 +0000

    all of terraform

---
+++ terraform/dns.tf
@@ -0,0 +1,41 @@
+resource "aws_route53_zone" "private" {
+  name = "ceph.internal"
+
+  vpc {
+    vpc_id = aws_vpc.ceph.id
+  }
+}
+
+resource "aws_route53_record" "management" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "management.ceph.internal"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.mon1.private_ip]
+}
+
+
+resource "aws_route53_record" "mon1" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "mon1.ceph.internal"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.mon1.private_ip]
+}
+
+resource "aws_route53_record" "mon2" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "mon2.ceph.internal"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.mon2.private_ip]
+}
+
+resource "aws_route53_record" "mon3" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "mon3.ceph.internal"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.mon3.private_ip]
+}
+
